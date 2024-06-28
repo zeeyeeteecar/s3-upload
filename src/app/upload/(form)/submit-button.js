6442d79585b76351bced73891df5f9ee7e612c19
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2024-06-28 16:35:29 -0700

    2

--- src/app/upload/(form)/submit-button.js
+++ src/app/upload/(form)/submit-button.js
@@ -5,8 +5,13 @@ import { useFormStatus } from "react-dom";
 
 export function SubmitButton() {
   const { pending } = useFormStatus();
+  
   return (
-    <button type="submit" className="submit-button" aria-disabled={pending}>
+    <button
+      type="submit"
+      className="w-[300px] h-[60px] bg-slate-50 rounded-full border-2 text-slate-600"
+      aria-disabled={pending}
+    >
       Upload File {pending && "ing..."}
     </button>
   );
--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -4,9 +4,9 @@ import {UploadForm} from "@/app/upload/(form)/form"
 
 export default function page() {
   return (
-    <>
+    <div className="w-screen h-screen bg-slate-300">
       <h1>Upload Files to S3 Bucket</h1>
       <UploadForm />
-    </>
+    </div>
   );
 }
