e8ec77c5625742bab237456cfc25974fa61586bb
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2024-07-03 14:47:20 -0700

    2

--- src/app/listFiles/page.tsx
+++ src/app/listFiles/page.tsx
@@ -1,8 +1,14 @@
 import ImageGallery from "./components/ImageGallery";
 import ListObject from "./components/ListObject";
 import { UploadForm } from "@/app/listFiles/(form)/form";
+import { revalidatePath } from "next/cache";
+
 
 export default function Home() {
+
+  revalidatePath("/listFiles");
+
+
   return (
     <div>
       <h1>Image Gallery</h1>
